788e41acef4eca8107ec7581bbee2c1aac28427e
Ashley <thy.ashley@gmail.com>
2020-10-24 18:54:12 +0800

    added paycard options

--- src/screen/payTransfer.js
+++ src/screen/payTransfer.js
@@ -1,19 +1,26 @@
-import React, { useContext, useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import React, { useContext, useState, useEffect, Fragment } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ScrollView,
+} from "react-native";
 
 import color from "../config/color";
 import { getAccountDetailsById } from "../../mockdata";
 import { AuthContext } from "../context/AuthContext";
 import InfoCard from "../components/InfoCard";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { withSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function payTransfer({ to = "" }) {
   const { uid, logout } = useContext(AuthContext);
   const [bank, setBank] = useState([]);
   const [creditCard, setCreditCard] = useState([]);
   const [selectedBank, setSelectedBank] = useState("");
+  const [selectedCard, setSelectedCard] = useState("");
   const [displayFund, setDisplayFund] = useState(true);
+  const [displayCard, setDisplayCard] = useState(true);
   const [transferType, setTransferType] = useState(null);
 
   useEffect(() => {
@@ -33,76 +40,168 @@ export default function payTransfer({ to = "" }) {
           source={require("../../assets/transferMoney.png")}
         />
       </View>
-
-      <View style={styles.header}>
-        <Text
-          style={{
-            marginLeft: 20,
-            fontSize: 16,
-            fontWeight: "500",
-            marginVertical: 10,
-          }}
-        >
-          Funds
-        </Text>
-        <View
-          style={{
-            flexGrow: 1,
-            alignItems: "flex-end",
-            marginRight: 20,
-          }}
-        >
-          <Text onPress={() => setDisplayFund(true)} style={styles.btn}>
-            Select Fund
+      <ScrollView>
+        <View style={styles.header}>
+          <Text
+            style={{
+              marginLeft: 20,
+              fontSize: 16,
+              fontWeight: "500",
+              marginVertical: 10,
+            }}
+          >
+            Funds
           </Text>
+          <View
+            style={{
+              flexGrow: 1,
+              alignItems: "flex-end",
+              marginRight: 20,
+            }}
+          >
+            <Text onPress={() => setDisplayFund(true)} style={styles.btn}>
+              Select Fund
+            </Text>
+          </View>
         </View>
-      </View>
-
-      <View style={styles.fundContainer}>
-        {!displayFund && selectedBank && (
-          <InfoCard
-            key={selectedBank.accountNumber}
-            title={selectedBank.bankName}
-            accNumber={selectedBank.accountNumber}
-            amount={selectedBank.amount}
-            text="Available Balance"
-          />
-        )}
-
-        {displayFund &&
-          bank?.map((b) => (
+        <View style={styles.fundContainer}>
+          {!displayFund && selectedBank && (
             <InfoCard
-              key={b.accountNumber}
-              title={b.bankName}
-              accNumber={b.accountNumber}
-              amount={b.amount}
+              key={selectedBank.accountNumber}
+              title={selectedBank.bankName}
+              accNumber={selectedBank.accountNumber}
+              amount={selectedBank.amount}
               text="Available Balance"
-              callToAction="SELECT"
-              ctAction={() => {
-                setDisplayFund(false);
-                setSelectedBank(b);
-              }}
             />
-          ))}
-      </View>
-      <View style={styles.paymentContainer}>
-        <View
-          style={[
-            styles.paymentOptionContainer,
-            { backgroundColor: color.primary },
-          ]}
-        >
-          <Text>Pay Card</Text>
+          )}
+
+          {displayFund &&
+            bank?.map((b) => (
+              <InfoCard
+                key={b.accountNumber}
+                title={b.bankName}
+                accNumber={b.accountNumber}
+                amount={b.amount}
+                text="Available Balance"
+                callToAction="SELECT"
+                ctAction={() => {
+                  setDisplayFund(false);
+                  setSelectedBank(b);
+                }}
+              />
+            ))}
         </View>
-        <View
-          style={[
-            styles.paymentOptionContainer,
-            { backgroundColor: color.secondary },
-          ]}
-        >
-          <Text>Transfer</Text>
+        <View style={styles.paymentContainer}>
+          <View
+            style={[
+              styles.paymentOptionContainer,
+              { backgroundColor: color.primary },
+            ]}
+          >
+            <Text
+              style={{ width: "100%", textAlign: "center" }}
+              onPress={() => setTransferType("card")}
+            >
+              Pay Card
+            </Text>
+          </View>
+          <View
+            style={[
+              styles.paymentOptionContainer,
+              { backgroundColor: color.secondary },
+            ]}
+          >
+            <Text
+              style={{ width: "100%", textAlign: "center" }}
+              onPress={() => setTransferType("nonCard")}
+            >
+              Transfer
+            </Text>
+          </View>
         </View>
-      </View>
+        {transferType === "card" ? (
+          <Fragment>
+            <View style={styles.header}>
+              <Text
+                style={{
+                  marginLeft: 20,
+                  fontSize: 16,
+                  fontWeight: "500",
+                  marginVertical: 10,
+                }}
+              >
+                Card
+              </Text>
+              <View
+                style={{
+                  flexGrow: 1,
+                  alignItems: "flex-end",
+                  marginRight: 20,
+                }}
+              >
+                <Text onPress={() => setDisplayCard(true)} style={styles.btn}>
+                  Select Card
+                </Text>
+              </View>
+            </View>
+            <View style={styles.fundContainer}>
+              {!displayCard && selectedCard && (
+                <InfoCard
+                  key={selectedCard.accountNumber}
+                  title={selectedCard.accountType}
+                  accNumber={selectedCard.cardNumber}
+                  amount={selectedCard.amount}
+                  text="Outstanding Amount"
+                />
+              )}
+              {displayCard &&
+                creditCard?.map((c) => (
+                  <InfoCard
+                    key={c.cardId}
+                    title={c.accountType}
+                    accNumber={c.cardNumber}
+                    amount={c.amount}
+                    text="Outstanding Amount"
+                    callToAction="PAY"
+                    ctAction={() => {
+                      setDisplayCard(false);
+                      setSelectedCard(c);
+                    }}
+                  />
+                ))}
+            </View>
+          </Fragment>
+        ) : null}
+        {selectedCard
+          ? setSelectedBank && (
+              <View
+                style={{
+                  width: "100%",
+                  height: 40,
+                  margin: 20,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <Text
+                  style={[
+                    styles.btn,
+                    {
+                      padding: 15,
+                      textAlign: "center",
+                      backgroundColor: color.secondary,
+                      color: "black",
+                    },
+                  ]}
+                >
+                  Make Payment
+                </Text>
+              </View>
+            )
+          : null}
+        <View style={{ height: 20 }} />
+      </ScrollView>
     </View>
   );
 }
